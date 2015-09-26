4d6eb0a1193811ea6f1099330554c79276d150b7
Michael Williamson <mike@zwobble.org>
2015-09-26 16:04:05 +0100

    Add conversion of empty files

---
+++ src/main/java/org/zwobble/mammoth/Mammoth.java
@@ -0,0 +1,11 @@
+package org.zwobble.mammoth;
+
+import java.io.InputStream;
+
+public class Mammoth {
+
+    public static String convertToHtml(InputStream stream) {
+        return "";
+    }
+
+}
--- src/test/java/org/zwobble/mammoth/tests/MammothTests.java
+++ src/test/java/org/zwobble/mammoth/tests/MammothTests.java
@@ -1,13 +1,21 @@
 package org.zwobble.mammoth.tests;
 
 import org.junit.Test;
+import org.zwobble.mammoth.Mammoth;
 
-import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
+import lombok.val;
 
 public class MammothTests {
     @Test
-    public void canReadTestData() {
-        assertThat(TestData.stream("/test-data/empty.docx"), notNullValue());
+    public void emptyParagraphsAreIgnoredByDefault() {
+        assertThat(convertToHtml("/test-data/empty.docx"), is(""));
+    }
+
+    private String convertToHtml(String name) {
+        val stream = TestData.stream(name);
+        return Mammoth.convertToHtml(stream);
     }
 }
