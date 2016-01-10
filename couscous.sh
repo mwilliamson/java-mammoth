5940b5961685346573684376b73ad841b0f214f5
Michael Williamson <mike@zwobble.org>
2016-01-10 15:09:46 +0000

    Add couscous.sh

---
+++ couscous.sh
@@ -0,0 +1,4 @@
+couscous --backend=python --output="$1" \
+    src/main/java/org/zwobble/mammoth/docx \
+    --sourcepath src/main/java/ \
+    --sourcepath ~/.m2/repository/com/google/guava/guava/18.0/guava-18.0-sources.jar
