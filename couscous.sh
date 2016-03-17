54c8f9dc4d0508fbe9489c6835621f13a41ced09
Michael Williamson <mike@zwobble.org>
2016-03-17 19:47:58 +0000

    Update package in couscous.sh

--- couscous.sh
+++ couscous.sh
@@ -1,4 +1,4 @@
 couscous --backend=python --output="$1" \
-    src/main/java/org/zwobble/mammoth/docx \
+    src/main/java/org/zwobble/mammoth/internal/docx \
     --sourcepath src/main/java/ \
     --sourcepath ~/.m2/repository/com/google/guava/guava/18.0/guava-18.0-sources.jar
