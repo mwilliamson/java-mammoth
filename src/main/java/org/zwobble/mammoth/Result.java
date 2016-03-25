e9ed8085ac7a808d7ad83b4b7e1d6be8a41a6c94
Michael Williamson <mike@zwobble.org>
2016-03-25 11:18:51 +0000

    Add Javadoc to Result

--- src/main/java/org/zwobble/mammoth/Result.java
+++ src/main/java/org/zwobble/mammoth/Result.java
@@ -2,7 +2,17 @@ package org.zwobble.mammoth;
 
 import java.util.Set;
 
+/**
+ * The result of converting a document.
+ */
 public interface Result<T> {
+    /**
+     * The generated value.
+     */
     T getValue();
+
+    /**
+     * Any warnings generated during the conversion.
+     */
     Set<String> getWarnings();
 }
