0610baf0b3464b49042b00f5518a501e9b11f3e3
Michael Williamson <mike@zwobble.org>
2016-03-28 16:13:51 +0100

    Remove Guava from couscous source path

--- couscous.sh
+++ couscous.sh
@@ -1,4 +1,3 @@
 couscous --backend=python --output="$1" \
     src/main/java/org/zwobble/mammoth/internal/docx \
-    --sourcepath src/main/java/ \
-    --sourcepath ~/.m2/repository/com/google/guava/guava/18.0/guava-18.0-sources.jar
+    --sourcepath src/main/java/
