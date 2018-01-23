0b5985ee2ea46d0cee65400224ba3e9326f1ec12
Michael Williamson <mike@zwobble.org>
2018-01-23 21:40:41 +0000

    Add test for missing main document part

--- src/test/java/org/zwobble/mammoth/tests/docx/DocumentReaderTests.java
+++ src/test/java/org/zwobble/mammoth/tests/docx/DocumentReaderTests.java
@@ -1,22 +1,26 @@
 package org.zwobble.mammoth.tests.docx;
 
 import org.junit.jupiter.api.Test;
+import org.zwobble.mammoth.internal.archives.Archive;
 import org.zwobble.mammoth.internal.archives.InMemoryArchive;
 import org.zwobble.mammoth.internal.documents.Document;
 import org.zwobble.mammoth.internal.docx.DocumentReader;
 import org.zwobble.mammoth.internal.results.InternalResult;
+import org.zwobble.mammoth.internal.util.PassThroughException;
 import org.zwobble.mammoth.internal.xml.NamespacePrefixes;
 import org.zwobble.mammoth.internal.xml.XmlWriter;
 
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.zwobble.mammoth.internal.util.Lists.list;
 import static org.zwobble.mammoth.internal.util.Maps.map;
 import static org.zwobble.mammoth.internal.xml.XmlNodes.element;
 import static org.zwobble.mammoth.internal.xml.XmlNodes.text;
 import static org.zwobble.mammoth.tests.ResultMatchers.isInternalSuccess;
 import static org.zwobble.mammoth.tests.documents.DocumentElementMakers.*;
+import static org.zwobble.mammoth.tests.util.MammothAsserts.assertThrows;
 
 public class DocumentReaderTests {
     private final NamespacePrefixes mainDocumentNamespaces = NamespacePrefixes.builder()
@@ -56,5 +60,26 @@ public class DocumentReaderTests {
         assertThat(result, isInternalSuccess(document(
             withChildren(paragraphWithText("Hello."))
         )));
+    }
+
+    @Test
+    public void errorIsThrownWhenMainDocumentPartDoesNotExist() {
+        Archive archive = InMemoryArchive.fromStrings(map(
+            "_rels/.rels", XmlWriter.toString(
+                element("Relationships", list(
+                    element("Relationship", map(
+                        "Id", "rId1",
+                        "Target", "/word/document2.xml",
+                        "Type", "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument"
+                    ))
+                )),
+                relationshipsNamespaces
+            )
+        ));
+        PassThroughException exception = assertThrows(
+            PassThroughException.class,
+            () -> DocumentReader.readDocument(Optional.empty(), archive)
+        );
+        assertThat(exception.getMessage(), equalTo("java.io.IOException: Could not find main document part. Are you sure this is a valid .docx file?"));
     }
 }
